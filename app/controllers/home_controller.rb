26d532e6ee035ef7179b5d9235bf4b2d5f2e717f
Evan R <eunoia.github@gmail.com>
2013-10-14 08:30:49 -0700

    Added default blog post
    This way the app will run if there is no initial blog post

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    @blog = Blog.where(name: 'landing').last
+    @blog = Blog.where(name: 'landing').last||Blog.new({body:"Hello World, please create a new blog post named 'landing'"})
   end
 
   def dashboard
