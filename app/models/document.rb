9d8a4ad1dadc55101a73b5416e3058251b627e6f
Evan R <eunoia.github@gmail.com>
2013-10-14 19:29:21 -0700

    Added doc model validations

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,3 +1,10 @@
 class Document < ActiveRecord::Base
   belongs_to :user
+
+  validates :case_number, presence: true
+  validates :case_number, uniqueness: {message: "Someone has already submitted this case number, sorry"}
+  validates :filepicker_url, presence: true
+  validates :date_opened, presence: true
+  validates :case_name, presence: true
+
 end
